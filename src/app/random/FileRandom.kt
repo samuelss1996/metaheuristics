a79b69b1d2e24ae9359324d489c1a5b7f8e882eb
Unknown <samuelsoutullosobral@gmail.com>
2017-11-21 09:48:32 +0100

    Fixed locale problems when parsing doubles

--- src/app/random/FileRandom.kt
+++ src/app/random/FileRandom.kt
@@ -1,9 +1,12 @@
 package app.random
 
 import java.io.File
+import java.text.NumberFormat
+import java.util.*
 
 class FileRandom(private val file: File) : IRandom {
     private val reader = this.file.bufferedReader()
+    private val numberParser = NumberFormat.getInstance(Locale.US)
 
-    override fun next() = this.reader.readLine()?.toDouble() ?: throw NoSuchElementException("Leidos más números de los neecesarios")
+    override fun next() = this.numberParser.parse(this.reader.readLine()).toDouble()
 }
