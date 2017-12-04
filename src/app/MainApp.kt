00b91cb8237d2c1398001e29c569c66547dfc541
Unknown <samuelsoutullosobral@gmail.com>
2017-12-04 03:56:20 +0100

    Working everything for 3.1. Pending README and compile.sh

--- src/app/MainApp.kt
+++ src/app/MainApp.kt
@@ -5,10 +5,18 @@ import app.random.IRandom
 import app.random.StandardRandom
 import app.util.Cities
 import java.io.File
+import java.text.DecimalFormat
+import java.util.*
+import kotlin.math.floor
+import kotlin.math.ln
 
-const val TOTAL_ITERATIONS = 10000
-const val TABOO_CAPACITY = 100
-const val MAX_ITERATIONS_WITHOUT_IMPROVEMENT = 100
+const val MU = 0.01
+const val PHI = 0.5
+const val MAX_ITERATIONS = 10000
+const val MAX_TESTED_CANDIDATES = 80
+const val MAX_ACCEPTED_CANDIDATES = 20
+
+data class Candidate(val solution: List<Int>, val cost: Int)
 
 fun main(args: Array<String>) {
     when(args.size) {
@@ -24,9 +32,67 @@ fun main(args: Array<String>) {
 
 class Main(distancesFile: String, private val random: IRandom = StandardRandom()) {
     private val cities = Cities(File(distancesFile))
+    private var initialTemperature = 0.0
 
     fun run() {
-        this.generateInitialSolution()
+        var currentSolution = this.generateInitialSolution()
+        var currentCost = this.cities.getCost(currentSolution)
+        var bestSolution = currentSolution
+        var bestCost = currentCost
+        var temperature = this.initialTemperature
+        var iterations = 0
+        var bestIteration = 0
+        var testedCandidates = 0
+        var acceptedCandidates = 0
+        var coolingCount = 0
+
+        while(iterations < MAX_ITERATIONS) {
+            while(iterations < MAX_ITERATIONS && testedCandidates < MAX_TESTED_CANDIDATES && acceptedCandidates < MAX_ACCEPTED_CANDIDATES) {
+                iterations++
+                testedCandidates++
+                println("ITERACION: $iterations")
+
+                val (candidateSolution, candidateCost) = this.generateCandidateSolution(currentSolution)
+                val delta = candidateCost - currentCost
+                val exponential = Math.pow(Math.E, -delta / temperature)
+
+                println("\tDELTA: $delta")
+                println("\tTEMPERATURA: ${temperature.toOutput()}")
+                println("\tVALOR DE LA EXPONENCIAL: ${exponential.toOutput()}")
+
+                if(this.random.next() < exponential || delta < 0) {
+                    currentSolution = candidateSolution
+                    currentCost = candidateCost
+                    acceptedCandidates++
+
+                    if(currentCost < bestCost) {
+                        bestSolution = currentSolution
+                        bestCost = currentCost
+                        bestIteration = iterations
+                    }
+
+                    println("\tSOLUCION CANDIDATA ACEPTADA")
+                }
+
+                println("\tCANDIDATAS PROBADAS: $testedCandidates, ACEPTADAS: $acceptedCandidates\n")
+            }
+
+            if(iterations < MAX_ITERATIONS) {
+                coolingCount++
+                temperature = this.initialTemperature / (1 + coolingCount)
+                testedCandidates = 0
+                acceptedCandidates = 0
+
+                println("============================\nENFRIAMIENTO: $coolingCount\n============================")
+                println("TEMPERATURA: ${temperature.toOutput()}\n")
+            } else {
+                println("\nMEJOR SOLUCION: ")
+                println("\tRECORRIDO: ${bestSolution.toOutput()} ")
+                println("\tFUNCION OBJETIVO (km): $bestCost")
+                println("\tITERACION: $bestIteration")
+                println("\tmu = $MU, phi = $PHI")
+            }
+        }
     }
 
     private fun generateInitialSolution(): List<Int> {
@@ -43,10 +109,56 @@ class Main(distancesFile: String, private val random: IRandom = StandardRandom()
             result.add(current)
         }
 
+        this.initialTemperature = (MU / -ln(PHI)) * this.cities.getCost(result)
+
         println("SOLUCION INICIAL:")
-        println("\tRECORRIDO: ${result.toString().replace("[","").replace("]","").replace(",","")} ")
-        println("\tFUNCION OBJETIVO (km): ${this.cities.getCost(result)}\n")
+        println("\tRECORRIDO: ${result.toOutput()} ")
+        println("\tFUNCION OBJETIVO (km): ${this.cities.getCost(result)}")
+        println("\tTEMPERATURA INICIAL: ${this.initialTemperature.toOutput()}\n")
 
         return result
     }
+
+    private fun generateCandidateSolution(currentSolution: List<Int>): Candidate {
+        val result = currentSolution.toMutableList()
+        val a = this.random.next()
+        val cityIndexToMove = floor(a * (this.cities.citiesCount - 1)).toInt()
+        var bestCost = Int.MAX_VALUE
+        var bestPosition = 0
+
+        val city = result.removeAt(cityIndexToMove)
+
+        for(i in (0..result.size) - cityIndexToMove) {
+            result.add(i, city)
+            val currentCost = this.cities.getCost(result)
+
+            if(currentCost < bestCost) {
+                bestCost = currentCost
+                bestPosition = i
+            }
+
+            result.removeAt(i)
+        }
+
+        result.add(bestPosition, city)
+
+        println("\tINDICE CIUDAD: $cityIndexToMove")
+        println("\tCIUDAD: $city")
+        println("\tINDICE INSERCION: $bestPosition")
+        println("\tRECORRIDO: ${result.toOutput()} ")
+        println("\tFUNCION OBJETIVO (km): $bestCost")
+
+        return Candidate(result, bestCost)
+    }
+
+    private fun Double.toOutput(): String? {
+        val format = DecimalFormat.getInstance(Locale.US)
+        format.maximumFractionDigits = 6
+        format.minimumFractionDigits = 6
+        format.isGroupingUsed = false
+
+        return format.format(this)
+    }
+
+    private fun List<Int>.toOutput() = this.toString().replace("[","").replace("]","").replace(",","")
 }
--- src/app/random/FileRandom.kt
+++ src/app/random/FileRandom.kt
@@ -1,12 +1,12 @@
 package app.random
 
 import java.io.File
+import java.lang.Double
 import java.text.NumberFormat
 import java.util.*
 
 class FileRandom(private val file: File) : IRandom {
     private val reader = this.file.bufferedReader()
-    private val numberParser = NumberFormat.getInstance(Locale.US)
 
-    override fun next() = this.numberParser.parse(this.reader.readLine()).toDouble()
+    override fun next() = Double.valueOf(this.reader.readLine())
 }
